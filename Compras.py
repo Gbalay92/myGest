81f610ee3807b0de78527b3bfc52b9dac9a404c6
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2023-02-09 13:14:48 +0100

    compras 'script'

---
+++ Compras.py
@@ -0,0 +1,6 @@
+from Nomina import crear_nominas
+from modulos.Utilities.Utilities import crear_objetos_compra, process
+if __name__=="__main__":
+    #crear_nominas()
+    lista=crear_objetos_compra()
+    process("compras_procesadas", "fecha", "iva", "total_sin_iva", lista)

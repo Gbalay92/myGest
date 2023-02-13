e7b43f07906557db691e4691875f06b9f8816da9
zalobalay8 <92026671+zalobalay8@users.noreply.github.com>
2023-02-13 13:05:05 +0100

    crear_objetos_venta

--- modulos/Utilities/Utilities.py
+++ modulos/Utilities/Utilities.py
@@ -1,6 +1,5 @@
 from modulos.models.Transaccion import Compra, Venta
 import openpyxl as op
-import xlwings as xw
 import os
 import formulas
 from datetime import datetime, timedelta
@@ -18,7 +17,7 @@ def process(nombre_fichero,header1,header2,header3,lista_a_procesar):
     wb.save(filepath)
     
 def crear_objetos_compra():
-    lista_compra = []
+    lista_compras = []
     path="Documentos/facturas/compras"
     filename="compras.xlsx"
     xl_model=formulas.ExcelModel().loads(os.path.join(path,filename)).finish()
@@ -34,31 +33,42 @@ def crear_objetos_compra():
             iva=solution.get(cell_ref_iva).values[cell_ref_iva]
             for f in fecha:
                 if str(type(f))=="<class 'numpy.ndarray'>":
-                    lista_compra.append(Compra(xldate_to_datetime(f[0][0]).strftime("%d/%m/%Y"), iva[-1][0][0], total[-1][0][0]))
+                    lista_compras.append(Compra(xldate_to_datetime(f[0][0]).strftime("%d/%m/%Y"), iva[-1][0][0], total[-1][0][0]))
             row+=1
         except AttributeError:
             break
-    return lista_compra
+    return lista_compras
 
 
         
 def crear_objetos_venta():
-    xw.App(visible=False)
     lista_ventas = []
     dir_path="Documentos/facturas/ventas"
-    for path in os.listdir(dir_path): 
-        book=xw.Book(os.path.join(dir_path,path))
-        sheet=xw.sheets['factura']
-        fecha=sheet['H3'].value.strftime("%d/%m/%Y")
-        iva=sheet['F48'].value
-        total=sheet['F47'].value
-        venta=Venta(fecha, iva, total)
-        lista_ventas.append(venta)
-        #print(fecha, iva, total)
-        xw.App().quit()
+    for file in os.listdir(dir_path): 
+        cell_ref_total=f"'[{file}]FACTURA'!F47"
+        cell_ref_fecha=f"'[{file}]FACTURA'!H3"
+        cell_ref_iva=f"'[{file}]FACTURA'!F48"
+        xl_model=formulas.ExcelModel().loads(os.path.join(dir_path,file)).finish()
+        solution=xl_model.calculate()
+        total=solution.get(cell_ref_total).values[cell_ref_total]
+        fecha=solution.get(cell_ref_fecha).values[cell_ref_fecha]
+        iva=solution.get(cell_ref_iva).values[cell_ref_iva]
+        gasto_mes=total[-1][0][0]
+        f=fecha[-1][0][0]
+        i=iva[-1][0][0]
+        lista_ventas.append(Venta(xldate_to_datetime(f).strftime("%d/%m/%Y") , i, gasto_mes))
+        
+        #lista_ventas.append(Venta(fecha, iva, total))
     return lista_ventas
 
 def xldate_to_datetime(xldate):
 	temp = datetime(1899, 12, 30)
 	delta = timedelta(days=xldate)
 	return temp+delta
+
+def write_gerencia(cantidad, celda):
+    file="Documentos/gerencia/informe-xerencia.xlsx"
+    wb = op.load_workbook(file)
+    ws=wb.active
+    ws[f'{celda}']=cantidad
+    wb.save(file)
